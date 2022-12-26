8fdd73f21358cfe5d92f4a412fc37b93bce88859
Moliki-Salman <salmanameenat@gmail.com>
2022-12-26 22:23:19 +0100

    introduction to javascript

--- java_practice.js
+++ java_practice.js
@@ -1,1 +1,33 @@
 console.log("first Java practice with github");
+/* three ways to declare a variable in  Java*/
+// var var is used through out the program. Var can be changes 
+var myName = "Damilola";
+//let: let is only used with in the scope of where you declare them
+let  ourName = "Meenadcode";
+//const: const is a variable that should never change 
+const n = 2.5;
+var a = 10
+console.log(a);
+var sum = 10 + 3;
+console.log(sum);
+//incrementing number: this is when you add 1 to a number
+var myVar = 45
+myVar = myVar + 1;
+
+/* a = a + 12; is d same as using the += operator: a+= 12;
+b = 9 + b; is d same as using the += operator: b+= 9;
+c = c + 7;  is d same as using the += operator: c+= 7;
+the above example is both adding and assigning  value to the variable 
+NOTE: this short cut is also applicable to *= and /= */
+
+
+/* In Java  quotation  mark “ ” is needed to write a string e,g var myLastName = “Moliki”. 
+However if you have many quotation mark in a string eg “my name is” Damilola Moliki” Salman”;
+it is important to add the back slash mark \ before each string so that JS can interpret it 
+properly as one single string like this:  
+var myStr =   “my name is \” Damilola Moliki \” Salman”;
+
+
+
+
+
